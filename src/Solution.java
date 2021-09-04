5e2c9f5fd4f4f55411e8425aab988e63ff1a680c
wjxor <shko940575@gmail.com>
2021-09-05 05:01:21 +0900

    행렬의 덧셈

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,13 @@
-// 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
-// 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+// 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 
+// 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
 class Solution {
-	public long[] solution(int x, int n) {
-		long[] answer = new long[n];
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr1[0].length];
 
-		for (long i = 0; i < n; i++) {
-
-			answer[(int) i] = x * (i + 1);
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[0].length; j++) {
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
 		}
 
 		return answer;
