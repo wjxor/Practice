7f3bbf11abf52c4d382261f74e579a8a29c62b5a
wjxor <shko940575@gmail.com>
2021-09-05 06:27:46 +0900

    하샤드 수

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,21 @@
-//전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 
-//전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요
+//양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
 class Solution {
-	public String solution(String phone_number) {
-		String answer = "";
-		String[] a = phone_number.split("");
+	public boolean solution(int x) {
+		boolean answer = true;
 
-		for (int i = 0; i < a.length; i++) {
-			if (i < (a.length - 4)) {
-				answer += "*";
-			} else {
-				answer += a[i];
-			}
+		int sum = 0;
+		int TF = x;
+
+		while (x >= 1) {
+			sum += TF % 10;
+			TF /= 10;
 		}
 
-		return answer;
+		if (x % sum == 0) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 }
