549cf500a5f6f30f66d1b1d97bc3ab40c012a3b5
wjxor <shko940575@gmail.com>
2021-09-06 00:47:22 +0900

    하샤드 수 수정

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,11 @@
 //양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
 class Solution {
 	public boolean solution(int x) {
-		boolean answer = true;
 
 		int sum = 0;
 		int TF = x;
 
-		while (x >= 1) {
+		while (TF >= 1) {
 			sum += TF % 10;
 			TF /= 10;
 		}
