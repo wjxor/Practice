3fb8c0888e3e162038ff829edb9a0c8743ab3020
wjxor <shko940575@gmail.com>
2021-09-07 13:19:30 +0900

    556:배열2 - 자가진단2

--- src/jungol/Main.java
+++ src/jungol/Main.java
@@ -1,22 +1,17 @@
 package jungol;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		char[] arr = new char[10];
+		int[] arr = new int[10];
 		String str = "";
 
-		Scanner sc = new Scanner(System.in);
-
 		for (int i = 0; i < arr.length; i++) {
-			str = sc.next();
-			arr[i] = str.charAt(0);
+			arr[i] = i + 1;
 		}
 
 		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]);
+			System.out.printf("%d ", arr[i]);
 		}
 
 	}
