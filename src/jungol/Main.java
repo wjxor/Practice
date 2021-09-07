4a7968bfe57630bf5c9c109c791ac5ef2f0d6525
wjxor <shko940575@gmail.com>
2021-09-07 13:23:08 +0900

    557:배열1 - 자가진단3

--- src/jungol/Main.java
+++ src/jungol/Main.java
@@ -1,18 +1,21 @@
 package jungol;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int[] arr = new int[10];
+		char[] arr = new char[10];
 		String str = "";
 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = i + 1;
-		}
+		Scanner sc = new Scanner(System.in);
 
 		for (int i = 0; i < arr.length; i++) {
-			System.out.printf("%d ", arr[i]);
+			str = sc.next();
+			arr[i] = str.charAt(0);
 		}
+
+		System.out.printf("%c %c %c", arr[0], arr[3], arr[6]);
 
 	}
 
